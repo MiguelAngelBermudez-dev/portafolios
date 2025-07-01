77b7b958f7b1233e5ac52bebc6f2a6a0bbd538df
Miguel <bermudezcotemiguelangel@gmail.com>
2025-07-01 12:22:24 +0200

    añadido imagen y texto principal

--- template/header.php
+++ template/header.php
@@ -11,14 +11,17 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <!--JS-->
   <script type="text/javascript" src="js/main.js"></script>
- 
+
   <!-- Load library from the CDN -->
   <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
   <!--GOOGLE FONT-->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Rock+Salt&family=Rubik+Moonrocks&family=Tauri&display=swap" rel="stylesheet">
-
+  <!--Nueva fuente-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 </head>
 
 <section id="welcome">
@@ -27,24 +30,24 @@
   </div>
 </section>
 <section class="layout">
-  <header>
+  <header class="z-[1]">
     <section>
-      <div><img  src="media/logo.png" alt="logo"></div>
-     
+      <div><img src="media/logo.png" alt="logo"></div>
+
       <div class="menu">
-      
+
         <label class="hamburger">
-    
-          <input type="checkbox" class="hamburgesa" >
-       
+
+          <input type="checkbox" class="hamburgesa">
+
           <svg viewBox="0 0 32 32">
             <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
             <path class="line" d="M7 16 27 16"></path>
           </svg>
-        
+
         </label>
-       
+
       </div>
-     
+
     </section>
   </header>
